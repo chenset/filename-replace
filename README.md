d354f229099774260e5f39beb252abcf0c4e176e
chenset <4199191@qq.com>
2016-10-29 22:01:50 +0800

    regex

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # filename-replace
 字幕文件重命名工具
+
+
+# todo list
+
+- 支持cli参数, 并判断参数正确
+- 支持中文路径
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <regex>
+#include <fstream>
 
 using namespace std;
 
@@ -46,7 +47,6 @@ bool inArray(string str, string *start, string *last) {
     return false;
 }
 
-
 void movieProcess(const string &file) {
     smatch what;
     auto start = file.begin();
@@ -60,7 +60,85 @@ void movieProcess(const string &file) {
 
 }
 
+vector<string> strSplit(const string &str, const string &split) {
+    vector<string> res;
+    size_t splitLen = split.length();
+    size_t pos;
+    string findStr = str;
+    do {
+        pos = findStr.find(split);
+        res.push_back(findStr.substr(0, pos));
+        findStr = findStr.substr(pos + splitLen);
+    } while (pos != string::npos);
+    return res;
+}
+
+bool inputMatched(string file, string inputName) {
+    //全部转为小写
+    transform(file.begin(), file.end(), file.begin(), ::tolower);
+    transform(inputName.begin(), inputName.end(), inputName.begin(), ::tolower);
+
+    smatch inputMatch;
+    if (!regex_search(inputName, inputMatch, regex("#+"))) {
+        return false;
+    }
+
+    vector<string> inputNameSplit = strSplit(inputName, inputMatch[0]);
+    if (inputNameSplit.size() == 0) {
+        return false;
+    }
+
+    for (int i = 0; i < inputNameSplit.size(); ++i) {
+        if (string::npos == file.find(inputNameSplit[i])) {
+            return false;
+        }
+    }
+
+
+    return true;
+}
+
+void subsProcess(const string &path, const string &file, const string &inputName, const string &outputName) {
+    if (!inputMatched(file, inputName)) {
+        return;
+    }
+
+    smatch outputMatch;
+    if (!regex_search(outputName, outputMatch, regex("#+"))) {
+        return;
+    }
+
+    smatch inputMatch;
+    if (!regex_search(inputName, inputMatch, regex("#+"))) {
+        return;
+    }
+
+    unsigned inputMatchLen = (unsigned) inputMatch[0].length();
+    string matchRepeat(inputMatchLen, '#');
+
+    string strNum = file.substr(inputName.find(matchRepeat), inputMatchLen);
+    string NewFilename = path + regex_replace(outputName, regex(matchRepeat), strNum);
+    std::cout << " ---------------------------------------------------------- " << endl;
+    std::cout << path + file + "\r\nrename to:\r\n" + NewFilename << endl;
+    rename((path + file).c_str(), NewFilename.c_str());
+
+    return;
+
+    smatch what;
+    auto start = file.begin();
+    auto end = file.end();
+
+    std::cout << file << endl;
+    while (regex_search(start, end, what, regex("\\d+"))) {
+        std::cout << what[0] << std::endl;
+        start += what.position() + what.length();
+    }
+
+}
+
 int main(void) {
+    string inputName = "[ANK-Raws] Guilty Crown - ## (BDrip 1920x1080 x264 FLAC Hi10P).ass";
+    string outputName = "Guilty Crown 2011 EP## [BD 1920x1080 23.976fps AVC-yuv420p10 FLACx2] - yan04000985&VCB-Studio.ass";
     string subPostfix[] = {"ssa", "ass", "smi", "str", "sub", "lrc", "sst", "txt", "xss", "psb", "ssb"};
     string moviePostfix[] = {"wmv", "asf", "asx", "rm", "rmvb", "mpg", "mpeg", "mpe", "3gp", "mov", "mp4", "m4v",
                              "avi",
@@ -71,8 +149,8 @@ int main(void) {
     vector<string> subs, movies;
 
     _finddata_t fileDir;
-    string path = "D:/test/";
-    char *dir = "D:/test/*";
+    string path = "D:/Guilty Crown/";
+    char *dir = "D:/Guilty Crown/*";
     int lfDir;
 
     if ((lfDir = _findfirst(dir, &fileDir)) != -1l) {
@@ -88,22 +166,24 @@ int main(void) {
             transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
 
             if (inArray(ext, begin(moviePostfix), end(moviePostfix))) {
-                movies.push_back(tmp);
+//                movies.push_back(tmp);
+                movies.push_back(fileDir.name);
             }
             if (inArray(ext, begin(subPostfix), end(subPostfix))) {
-                subs.push_back(tmp);
+//                subs.push_back(tmp);
+                subs.push_back(fileDir.name);
             }
         } while (_findnext(lfDir, &fileDir) == 0);
     }
     _findclose(lfDir);
 
 
-    std::cout << movies.size() << endl;
-    std::cout << subs.size() << endl;
-
-
     for (auto i = movies.begin(); i != movies.end(); ++i) {
-        movieProcess(*i);
+//        movieProcess(*i);
+    }
+
+    for (auto i = subs.begin(); i != subs.end(); ++i) {
+        subsProcess(path, *i, inputName, outputName);
     }
 
 }
